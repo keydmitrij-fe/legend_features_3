08bdd3a9be27321d5d2e198a4afb1db3f3e8a98d
Vitaliy Mitrofanov <mitrofanov2122@gmail.com>
2026-02-02 17:15:12 +0300

    feat: add React-Router to the project, make a "Profile" page, implement switching between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import { Layout } from 'antd';
-import TodoPage from './pages/TodoPage';
+import { Button, Flex, Layout } from 'antd';
 import Sider from 'antd/es/layout/Sider';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router';
+import TodoPage from './pages/TodoPage';
+import Profile from './pages/Profile';
+import { CarryOutOutlined, SmileOutlined } from '@ant-design/icons';
 
 function App() {
   return (
-    <Layout>
-      <Sider theme={'light'}></Sider>
-      <TodoPage />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Sider theme={'light'}>
+          <Flex vertical gap={10}>
+            <NavLink to={'/'} end>
+              <Button
+                icon={<CarryOutOutlined />}
+                variant={'solid'}
+                color={'primary'}
+                size={'large'}
+              >
+                Список задач
+              </Button>
+            </NavLink>
+            <NavLink to={'/profile'}>
+              <Button
+                icon={<SmileOutlined />}
+                variant={'solid'}
+                color={'primary'}
+                size={'large'}
+              >
+                Профиль
+              </Button>
+            </NavLink>
+          </Flex>
+        </Sider>
+
+        <Routes>
+          <Route path={'/'} element={<TodoPage />}></Route>
+          <Route path={'profile'} element={<Profile />}></Route>
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { Empty } from 'antd';
+
+const Profile: FC = () => {
+  return <Empty description={'Привет!'} />;
+};
+
+export default Profile;
