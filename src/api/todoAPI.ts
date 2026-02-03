fc6eb09057383e781c810bfb7bb560354d41d510
Vitaliy Mitrofanov <mitrofanov2122@gmail.com>
2026-02-03 12:11:53 +0300

    chore: configure axios instance

--- src/api/todoAPI.ts
+++ src/api/todoAPI.ts
@@ -6,24 +6,26 @@ import type {
 } from '../types/todoTypes.ts';
 import axios from 'axios';
 
-const API = 'https://easydev.club/api/v1/todos';
+const instance = axios.create({
+  baseURL: 'https://easydev.club/api/v1',
+});
 
 export const getTodos = async (
   status: TodoStatus,
 ): Promise<MetaResponse<Todo, TodoInfo>> => {
-  const response = await axios.get(`${API}?filter=${status}`);
+  const response = await instance.get(`/todos?filter=${status}`);
 
   return response.data;
 };
 
 export async function addTodo(title: string) {
-  await axios.post(API, { isDone: false, title });
+  await instance.post('/todos', { isDone: false, title });
 }
 
 export async function deleteTodo(id: number) {
-  await axios.delete(`${API}/${id}`);
+  await instance.delete(`/todos/${id}`);
 }
 
 export async function editTodo(id: number, editedTodo: Todo): Promise<void> {
-  await axios.put(`${API}/${id}`, editedTodo);
+  await instance.put(`todos/${id}`, editedTodo);
 }
